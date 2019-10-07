bd4bb2f6bbbec125d88c72b1eb85bfc01fa643f5
rasputtim <rasputtim@gmx.com>
2019-10-07 16:43:18 -0300

    create database tables

--- db/myDb.sql
+++ db/myDb.sql
@@ -26,24 +26,24 @@ CREATE TABLE ezfin_transactions (
             dueDate TEXT,
             description TEXT,
             idCategory INTEGER NOT NULL,
-            amount DOUBLE,
+            amount REAL,
             paymentDate TEXT,
             status INTEGER,
             modificationDateTime timestamp without time zone DEFAULT CURRENT_TIMESTAMP(0),
             PRIMARY KEY (idTransaction, idUser),
-            FOREIGN KEY (idCategory) REFERENCES ezfin_category (idCat),
+            FOREIGN KEY (idCategory,idUser) REFERENCES ezfin_category (idCat,idUser),
             FOREIGN KEY (idUser) REFERENCES ezfin_tusuario (id_usuario)
             );
             
 CREATE TABLE ezfin_balanceView (
             idBalView  SERIAL NOT NULL,
             idUser varchar(50) NOT NULL,
-            initialDate TEXT
-            finalDate TEXT
-            keyDate TEXT
+            initialDate TEXT,
+            finalDate TEXT,
+            keyDate TEXT,
             description TEXT,
             title TEXT,
-            finalBalance DOUBLE,
+            finalBalance REAL,
             isCurrent INTEGER,
             PRIMARY KEY (idBalView, idUser),
             FOREIGN KEY (idUser) REFERENCES ezfin_tusuario (id_usuario)
