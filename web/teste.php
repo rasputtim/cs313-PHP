5fbbecf9fe3a1ac1f7a0168553f1359be0ac266a
rasputtim <rasputtim@gmx.com>
2019-10-07 15:02:45 -0300

    db connection page

--- web/teste.php
+++ web/teste.php
@@ -3,6 +3,12 @@
   <title>PHP Test</title>
  </head>
  <body>
- <?php echo '<p>Hello World</p>'; ?> 
+ <?php
+$dbUrl = getenv('DATABASE_URL');
+?>
+ <?php echo '<p>Hello World</p>'; 
+       echo "$dbUrl";
+?> 
+ 
  </body>
 </html>
