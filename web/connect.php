4c1358edfd754d6644592166f747b3625218ab9d
rasputtim <rasputtim@gmx.com>
2019-10-07 15:24:14 -0300

    db connection page

--- web/connect.php
+++ web/connect.php
@@ -3,31 +3,33 @@
   <title>PHP Test</title>
  </head>
  <body>
-<? php 
+ <?php
 try
 {
-  $dbUrl = getenv('DATABASE_URL');
-
-  $dbOpts = parse_url($dbUrl);
-
-  $dbHost = $dbOpts["host"];
+$dbUrl = getenv('DATABASE_URL');
+echo "$dbUrl";
+$dbOpts = parse_url($dbUrl);
+$dbHost = $dbOpts["host"];
   $dbPort = $dbOpts["port"];
   $dbUser = $dbOpts["user"];
   $dbPassword = $dbOpts["pass"];
   $dbName = ltrim($dbOpts["path"],'/');
-
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 }
 catch (PDOException $ex)
 {
-	echo '<p> Error!: ' . $ex->getMessage();
-       echo "</p>";
+  echo 'Error!: ' . $ex->getMessage();
   die();
 }
-echo "<p> SUCCESS </p>";
-?>
-<?php echo $dbUrl; ?>  
-</body>
+  echo "<p> Host: $dbHost";
+  echo "<p> User: $dbUser";
+  echo "<p> Password: $dbPassword";
+  echo "<p> Port: $dbPort";
+  echo "<p> Database Name: $dbName";
+
+?> 
+ 
+ </body>
 </html>
