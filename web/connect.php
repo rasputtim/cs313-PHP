ab684cd30c1b48527abdb2a23cee55a538915d21
rasputtim <rasputtim@gmx.com>
2019-10-07 14:54:29 -0300

    db connection page

--- web/connect.php
+++ web/connect.php
@@ -1,3 +1,8 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
 <? php 
 try
 {
@@ -14,11 +19,13 @@ try
   $db = new PDO("pgsql:host=$dbHost;port=$dbPort;dbname=$dbName", $dbUser, $dbPassword);
 
   $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-echo 'SUCCESS';
 }
 catch (PDOException $ex)
 {
   echo 'Error!: ' . $ex->getMessage();
   die();
 }
+echo "<p> SUCCESS </p>";
 ?>
+ </body>
+</html>
