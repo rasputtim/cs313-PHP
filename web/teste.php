e4cf4e8590b3d612eda292883bf885f110b1c2e0
rasputtim <rasputtim@gmx.com>
2019-10-07 15:07:12 -0300

    db connection page

--- web/teste.php
+++ web/teste.php
@@ -7,7 +7,18 @@
 $dbUrl = getenv('DATABASE_URL');
 ?>
  <?php echo '<p>Hello World</p>'; 
-       echo "$dbUrl";
+echo "$dbUrl";
+$dbOpts = parse_url($dbUrl);
+$dbHost = $dbOpts["host"];
+  $dbPort = $dbOpts["port"];
+  $dbUser = $dbOpts["user"];
+  $dbPassword = $dbOpts["pass"];
+  $dbName = ltrim($dbOpts["path"],'/');
+  echo "Host: $dbHost";
+  echo "User: $dbUser";
+  echo "Password: $dbPassword";
+  echo "Port: $dbPort";
+  echo "Database Name: $dbName";
 ?> 
  
  </body>
