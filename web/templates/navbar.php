de4c430ab2b51a2df3ffeb739e599ec4dd754043
rasputtim <rasputtim@gmx.com>
2019-10-08 09:28:09 -0300

    navbar

--- web/templates/navbar.php
+++ web/templates/navbar.php
@@ -8,6 +8,11 @@ if ($index3=="true"){
 }else{
     $index3_active = "";
 }
+if ($index2=="true"){
+    $index3_active = 'active';
+}else{
+    $index3_active = "";
+}
 ?>
 
 <div class="navbar navbar_">
@@ -19,33 +24,33 @@ if ($index3=="true"){
 		</a>
 		<div class="nav-collapse nav-collapse_ collapse">
 			<ul class="nav sf-menu clearfix">
-      <li><a href="index.html">Home</a></li>
-      <li><a href="index-1.html">About Us</a></li>
-				<li class="sub-menu sub-menu-1"><a href="index-2.html">Products</a>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="index-1.php">About Us</a></li>
+				<li class="<?php echo $index2_active; ?> sub-menu sub-menu-1"><a href="index-2.php">Products</a>
 					<ul>
-						<li><a href="index-2.html">what we offer</a></li>
-						<li class="sub-menu sub-menu-2"><a href="index-2.html">specials</a>
+						<li><a href="index-2.php">what we offer</a></li>
+						<li class="sub-menu sub-menu-2"><a href="index-2.php">specials</a>
 							<ul>
-								<li><a href="index-2.html">lorem ipsum dolor</a></li>
-								<li><a href="index-2.html">sit amet</a></li>
-								<li><a href="index-2.html">adipiscing elit</a></li>
-								<li><a href="index-2.html">nunc suscipit</a></li>
-								<li><a href="404.html">404 page not found</a></li>
+								<li><a href="index-2.php">lorem ipsum dolor</a></li>
+								<li><a href="index-2.php">sit amet</a></li>
+								<li><a href="index-2.php">adipiscing elit</a></li>
+								<li><a href="index-2.php">nunc suscipit</a></li>
+								<li><a href="404.php">404 page not found</a></li>
 							</ul>
 						</li>
-						<li class="sub-menu sub-menu-2"><a href="index-2.html">price list</a>
+						<li class="sub-menu sub-menu-2"><a href="index-2.php">price list</a>
 							<ul>
-								<li><a href="index-2.html">lorem ipsum dolor</a></li>
-								<li><a href="index-2.html">sit amet</a></li>
-								<li><a href="index-2.html">adipiscing elit</a></li>
-								<li><a href="index-2.html">nunc suscipit</a></li>									
+								<li><a href="index-2.php">lorem ipsum dolor</a></li>
+								<li><a href="index-2.php">sit amet</a></li>
+								<li><a href="index-2.php">adipiscing elit</a></li>
+								<li><a href="index-2.php">nunc suscipit</a></li>									
 							</ul>
 						</li>						
 					</ul>						
 				</li>
-				<li <?php echo $index3_active; ?> ><a href="index-3.html">Services</a></li>
-				<li><a href="index-4.html">Projects</a></li>
-				<li><a href="index-5.html">Contacts</a></li>											
+				<li <?php echo $index3_active; ?> ><a href="index-3.php">Services</a></li>
+				<li><a href="index-4.php">Projects</a></li>
+				<li><a href="index-5.php">Contacts</a></li>											
     </ul>
 		</div>
 	</div>
