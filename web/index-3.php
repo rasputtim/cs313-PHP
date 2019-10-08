fa784229f6f2a2d43a2272d4169083a3dd39e87c
rasputtim <rasputtim@gmx.com>
2019-10-08 09:14:20 -0300

    navbar

--- web/index-3.php
+++ web/index-3.php
@@ -10,7 +10,7 @@
 <div class="top1_inner clearfix">
 <div class="top1_inner_bg"></div>
 <header><div class="logo_wrapper"><a href="index.html" class="logo"><img class="llogo" src="images/logo.png" alt=""></a></div></header>
-<?php include('templates/navbar.php?index3="true"'); ?>
+<?php include('templates/navbar.php'); ?>
 </div>	
 </div>	
 </div>	
