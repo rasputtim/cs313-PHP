11e8e6836127d511768c43498697c39abd9d4f0a
rasputtim <rasputtim@gmx.com>
2019-10-08 10:24:29 -0300

    navbar

--- web/index-1.php
+++ web/index-1.php
@@ -10,46 +10,11 @@
 <div class="top1_inner clearfix">
 <div class="top1_inner_bg"></div>
 <header><div class="logo_wrapper"><a href="index.html" class="logo"><img class="llogo" src="images/logo.png" alt=""></a></div></header>
-<div class="navbar navbar_">
-	<div class="navbar-inner navbar-inner_">
-		<a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</a>
-		<div class="nav-collapse nav-collapse_ collapse">
-			<ul class="nav sf-menu clearfix">
-      <li><a href="index.html">Home</a></li>
-      <li class="active"><a href="index-1.html">About Us</a></li>
-				<li class="sub-menu sub-menu-1"><a href="index-2.html">Products</a>
-					<ul>
-						<li><a href="404.html">404 page not found</a></li>
-						<li class="sub-menu sub-menu-2"><a href="index-2.html">specials</a>
-							<ul>
-								<li><a href="404.html">404 page not found</a></li>
-								<li><a href="index-2.html">sit amet</a></li>
-								<li><a href="index-2.html">adipiscing elit</a></li>
-								<li><a href="index-2.html">nunc suscipit</a></li>
-								<li><a href="404.html">404 page not found</a></li>
-							</ul>
-						</li>
-						<li class="sub-menu sub-menu-2"><a href="index-2.html">price list</a>
-							<ul>
-								<li><a href="index-2.html">lorem ipsum dolor</a></li>
-								<li><a href="index-2.html">sit amet</a></li>
-								<li><a href="index-2.html">adipiscing elit</a></li>
-								<li><a href="index-2.html">nunc suscipit</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li><a href="index-3.html">Services</a></li>
-				<li><a href="index-4.html">Projects</a></li>
-				<li><a href="index-5.html">Contacts</a></li>											
-    </ul>
-		</div>
-	</div>
-</div>
+
+<?php 
+$index1="true";
+include('templates/navbar.php'); ?>
+
 </div>	
 </div>	
 </div>	
--- web/templates/navbar.php
+++ web/templates/navbar.php
@@ -2,7 +2,11 @@
 
 <?php
 
-
+if ($index1=="true"){
+    $index1_active = 'class="active"';
+}else{
+    $index1_active = "";
+}
 if ($index3=="true"){
     $index3_active = 'class="active"';
 }else{
@@ -25,7 +29,7 @@ if ($index2=="true"){
 		<div class="nav-collapse nav-collapse_ collapse">
 			<ul class="nav sf-menu clearfix">
       <li><a href="index.php">Home</a></li>
-      <li><a href="index-1.php">About Us</a></li>
+      <li <?php echo $index1_active; ?> ><a href="index-1.php">About Us</a></li>
 				<li class="<?php echo $index2_active; ?> sub-menu sub-menu-1"><a href="index-2.php">Products</a>
 					<ul>
 						<li><a href="index-2.php">what we offer</a></li>
@@ -49,8 +53,8 @@ if ($index2=="true"){
 					</ul>						
 				</li>
 				<li <?php echo $index3_active; ?> ><a href="index-3.php">Services</a></li>
-				<li><a href="index-4.php">Projects</a></li>
-				<li><a href="index-5.php">Contacts</a></li>											
+				<li <?php echo $index4_active; ?> ><a href="index-4.php">Projects</a></li>
+				<li <?php echo $index5_active; ?> ><a href="index-5.php">Contacts</a></li>											
     </ul>
 		</div>
 	</div>
