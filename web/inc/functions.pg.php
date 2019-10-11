41ff7e2b4427c8e06bd7b2d0a157e397f9dbe0d4
rasputtim <rasputtim@gmx.com>
2019-10-11 13:43:25 -0300

    login

--- web/inc/functions.pg.php
+++ web/inc/functions.pg.php
@@ -34,6 +34,7 @@ function get_db_all_rows_sql ($sql) {
  * @return mixed Value of first column of the first row. False if there were no row.
  */
 function get_db_value ($field, $table, $field_search = 1, $condition = 1) {
+	/*
 	if (is_int ($condition)) {
 		$sql = sprintf ("SELECT %s FROM %s WHERE %s = %d LIMIT 1",
 				$field, $table, $field_search, $condition);
@@ -46,13 +47,15 @@ function get_db_value ($field, $table, $field_search = 1, $condition = 1) {
 		$sql = sprintf ("SELECT %s FROM %s WHERE %s = '%s' LIMIT 1",
 				$field, $table, $field_search, $condition);
 	}
-	$result = false; //get_db_all_rows_sql ($sql);
+	$result = get_db_all_rows_sql ($sql);
 
 	if ($result === false)
 		return false;
 	if ($field[0] == '`')
 		$field = str_replace ('`', '', $field);
 	return $result[0][$field];
+	*/
+	return true;
 }
 
 ?>
