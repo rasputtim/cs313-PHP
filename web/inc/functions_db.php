482037a5dc42ca8ca10c4a5f65c3c45b6fba193a
rasputtim <rasputtim@gmx.com>
2019-10-11 14:25:54 -0300

    login

--- web/inc/functions_db.php
+++ web/inc/functions_db.php
@@ -11,16 +11,18 @@ function check_login () {
 	if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
 		echo '<p>LOGGED IN SETTED</p>';
 	
-	}
-	if (isset ($_SESSION["id_usuario"])) {
-		$id = $_SESSION["id_usuario"];
-		$id_user = get_db_value ('id_usuario', 'public.ezfin.tusuario', 'id_usuario', $id);
-		echo '<p>INSIDE</p>';
-		echo '<p>usuario da sessão: '.$id.'</p>';
-		echo '<p>isiario do banco: '.$id_user.'</p>';
-		
-		if ($id == $id_user) {
-			return false;
+	
+		if (isset ($_SESSION["id_usuario"])) {
+			$id = $_SESSION["id_usuario"];
+			echo '<p>ID USUARIO  IN SETTED</p>';
+			$id_user = get_db_value ('id_usuario', 'public.ezfin.tusuario', 'id_usuario', $id);
+			echo '<p>INSIDE</p>';
+			echo '<p>usuario da sessão: '.$id.'</p>';
+			echo '<p>isiario do banco: '.$id_user.'</p>';
+			
+			if ($id == $id_user) {
+				return false;
+			}
 		}
 	}
 	echo '<p>usuario da sessão: '.$id.'</p>';
