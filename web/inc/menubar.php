528e61b8185822630a94a80424d3c9a2e64c2787
Salvatore <salvatore@byui.edu>
2019-10-12 08:28:30 -0300

    404

---
+++ web/inc/menubar.php
@@ -0,0 +1,15 @@
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="top1_inner clearfix">
+<div class="top1_inner_bg"></div>
+<header><div class="logo_wrapper"><a href="index.php" class="logo"><img class="llogo" src="images/logo.png" alt=""></a></div></header>
+<?php
+
+include('templates/navbar.php'); ?>
+
+</div>	
+</div>	
+</div>	
+</div>	
+</div>
