9517bf27a09a845e1d5e9ead8134545de22f3f67
Salvatore <salvatore@byui.edu>
2019-10-12 02:26:18 -0300

    404

--- web/index-5.php
+++ web/index-5.php
@@ -172,120 +172,16 @@ E-mail: <a href="#">mail@bteamny.com</a>
 </div>	
 </div>
 
-<div class="slogan1">
-<div class="container">
-<div class="row">
-<div class="span12">
-<div class="slogan1_inner clearfix">
-<div class="row">
-<div class="span8">
-<div class="block1">
-	<div class="txt1">Have Questions? <span>Call uS: 1 800 123 4567</span></div>
-	<div class="txt2">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut</div>
-</div>	
-</div>
-<div class="span4">
-<div class="block2">
-	<a href="#" class="button1">request free quote</a>
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>
 
-<div class="follow1">
-<div class="container">
-<div class="row">
-<div class="span12">
-<div class="follow1_wrapper clearfix">
-<div class="follow1_inner clearfix">
-<div class="block1">follow us and become our fan on</div>	
-<div class="block2"></div>
-<div class="block3">
-<div class="social_wrapper">
-	<ul class="social clearfix">
-	    <li><a href="#"><img src="images/social_ic1.png"></a></li>
-	    <li><a href="#"><img src="images/social_ic2.png"></a></li>
-	    <li><a href="#"><img src="images/social_ic3.png"></a></li>
-	    <li><a href="#"><img src="images/social_ic4.png"></a></li>
-	</ul>
-</div>	
-</div>
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>
 
 <div class="bot1">
 <div class="container">
 <div class="row">
 <div class="span12">
 <div class="bot1_inner">
-<div class="row">
-<div class="span3">
 
-	<div class="block_title">information</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Quisque nullatibulum libero</a></li>
-	  <li><a href="#">Scelerisque eget, malesuada at</a></li>
-	  <li><a href="#">Vivamus eget niiam cursus leo</a></li>
-	  <li><a href="#">Nulla facilisinean nec eros</a></li>
-	  <li><a href="#">Vestibulum ante ipsum</a></li>	                                                                                
-	  <li><a href="#">Primis in faucib</a></li>	                                                                                
-	</ul>
-
-</div>
-<div class="span3">
-
-	<div class="block_title">support</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Lorem ipsum dolor</a></li>
-	  <li><a href="#">Sit amet consectetue</a></li>
-	  <li><a href="#">Adipiscing elit</a></li>
-	  <li><a href="#">Nunc suscipit</a></li>
-	  <li><a href="#">Suspendisse enim arcu</a></li>	                                                                                	  
-	</ul>
-
-</div>
-<div class="span3">
-
-	<div class="block_title">downloads</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Convallis non cursus sed</a></li>
-	  <li><a href="#">Dignissim et est</a></li>
-	  <li><a href="#">Aenean semper</a></li>
-	  <li><a href="#">Aliquet libero</a></li>
-	  <li><a href="#">Lorem</a></li>	
-	</ul>
-
-</div>
-<div class="span3">
-
-	<div class="block_title">getting started</div>
-
-	<div class="phone1">
-		<div class="txt1">Have Questions? Call Us:</div>
-		<div class="txt2">1 800 123 456</div>
-	</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Our Location</a></li>
-	  <li><a href="#">Find an Agent</a></li>	                                                                                  
-	</ul>
-
-</div>	
-</div>
 	<?php include('templates/footer.php'); ?>
-</div>	
-</div>	
+	
 </div>	
 </div>	
 </div>
