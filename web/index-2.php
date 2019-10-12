ed0e0324506d0418c3ca08a4d7b5dfdab74fcc07
Salvatore <salvatore@byui.edu>
2019-10-12 02:13:20 -0300

    404

--- web/index-2.php
+++ web/index-2.php
@@ -256,54 +256,7 @@ if ($added == false) echo '</div>';
 </div>	
 </div>
 
-<div class="slogan1">
-<div class="container">
-<div class="row">
-<div class="span12">
-<div class="slogan1_inner clearfix">
-<div class="row">
-<div class="span8">
-<div class="block1">
-	<div class="txt1">Have Questions? <span>Call uS: 1 800 123 4567</span></div>
-	<div class="txt2">Lorem ipsum dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut</div>
-</div>	
-</div>
-<div class="span4">
-<div class="block2">
-	<a href="#" class="button1">request free quote</a>
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>
 
-<div class="follow1">
-<div class="container">
-<div class="row">
-<div class="span12">
-<div class="follow1_wrapper clearfix">
-<div class="follow1_inner clearfix">
-<div class="block1">follow us and become our fan on</div>	
-<div class="block2"></div>
-<div class="block3">
-<div class="social_wrapper">
-	<ul class="social clearfix">
-	    <li><a href="#"><img src="images/social_ic1.png"></a></li>
-	    <li><a href="#"><img src="images/social_ic2.png"></a></li>
-	    <li><a href="#"><img src="images/social_ic3.png"></a></li>
-	    <li><a href="#"><img src="images/social_ic4.png"></a></li>
-	</ul>
-</div>	
-</div>
-</div>	
-</div>	
-</div>	
-</div>	
-</div>	
-</div>
 
 <div class="bot1">
 <div class="container">
