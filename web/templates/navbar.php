55da65b8e1067a70899a806fadea18735f4c431f
Salvatore <salvatore@byui.edu>
2019-10-12 18:49:37 -0300

    database access

--- web/templates/navbar.php
+++ web/templates/navbar.php
@@ -66,14 +66,14 @@ if ($index5=="true"){
 				<li class="<?php echo $index4_active; ?>" ><a href="listviews.php">Views</a></li>
 				<li class="<?php echo $index5_active; ?>" ><a href="index-5.php">Contacts</a></li>	
 				<li class="<?php echo $index5_active; ?>" >
-					<form  action="login.php" method="post">
-						<div id="username" class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
-							<label class="col-form-label col-form-label-sm">Username</label>
+					<form class="form-inline" action="login.php" method="post">
+						<div class="form-group <?php echo (!empty($username_err)) ? 'has-error' : ''; ?>">
+							<label >Username</label>
 							<input type="text" name="username" class="form-control form-control-sm" value="<?php echo $username; ?>" placeholder = "username = admin">
 							<span class="help-block"><?php echo $username_err; ?></span>
 						</div>    
-						<div id="passwrd" class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-							<label class="col-form-label col-form-label-sm" >Password</label>
+						<div class="form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+							<label>Password</label>
 							<input type="password" name="password" class="form-control form-control-sm" placeholder = "password = 123456" required>
 							<span class="help-block"><?php echo $password_err; ?></span>
 						</div>
