35586a7cc846b69bd1900c0849218243abed258c
Salvatore <salvatore@byui.edu>
2019-10-12 02:10:10 -0300

    404

--- web/index-2.php
+++ web/index-2.php
@@ -310,66 +310,7 @@ if ($added == false) echo '</div>';
 <div class="row">
 <div class="span12">
 <div class="bot1_inner">
-<div class="row">
-<div class="span3">
-
-	<div class="block_title">information</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Quisque nullatibulum libero</a></li>
-	  <li><a href="#">Scelerisque eget, malesuada at</a></li>
-	  <li><a href="#">Vivamus eget niiam cursus leo</a></li>
-	  <li><a href="#">Nulla facilisinean nec eros</a></li>
-	  <li><a href="#">Vestibulum ante ipsum</a></li>	                                                                                
-	  <li><a href="#">Primis in faucib</a></li>	                                                                                
-	</ul>
-
-</div>
-<div class="span3">
-
-	<div class="block_title">support</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Lorem ipsum dolor</a></li>
-	  <li><a href="#">Sit amet consectetue</a></li>
-	  <li><a href="#">Adipiscing elit</a></li>
-	  <li><a href="#">Nunc suscipit</a></li>
-	  <li><a href="#">Suspendisse enim arcu</a></li>	                                                                                	  
-	</ul>
-
-</div>
-<div class="span3">
-
-	<div class="block_title">downloads</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Convallis non cursus sed</a></li>
-	  <li><a href="#">Dignissim et est</a></li>
-	  <li><a href="#">Aenean semper</a></li>
-	  <li><a href="#">Aliquet libero</a></li>
-	  <li><a href="#">Lorem</a></li>	
-	</ul>
-
-</div>
-<div class="span3">
-
-	<div class="block_title">getting started</div>
-
-	<div class="phone1">
-		<div class="txt1">Have Questions? Call Us:</div>
-		<div class="txt2">1 800 123 456</div>
-	</div>
-
-	<ul class="ul0">
-	  <li><a href="#">Our Location</a></li>
-	  <li><a href="#">Find an Agent</a></li>	                                                                                  
-	</ul>
-
-</div>	
-</div>
-	<?php include('templates/footer.php'); ?>
-</div>	
-</div>	
+<?php include('templates/footer.php'); ?>
 </div>	
 </div>	
 </div>
