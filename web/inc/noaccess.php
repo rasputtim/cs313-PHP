1b32fba615e65d9b35d31585e7703f40f2facb19
Salvatore <salvatore@byui.edu>
2019-10-12 00:46:10 -0300

    404

--- web/inc/noaccess.php
+++ web/inc/noaccess.php
@@ -4,7 +4,7 @@
 	echo '<div style="margin-top: 100px; width: 450px;">';
 	echo '<h2>'.'You don\'t have access to this page'.'</h2>';
 	echo "<p align='center'>";
-	echo "<img src='images/noaccess.gif'>";
+	echo "<img src='../images/noaccess.gif'>";
 	echo "<p>".'Access to this page is restricted to authorized users only, please contact system administrator if you need assistance. <br><br>Please know that all attempts to access this page are recorded in security logs of Integria System Database'. "</p>";
 	echo "</p>";
 	echo "</div>";
