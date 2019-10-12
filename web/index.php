8f4b175a7f30c7bf1b8765c65663ad58ce9cea75
Salvatore <salvatore@byui.edu>
2019-10-12 01:01:10 -0300

    404

--- web/index.php
+++ web/index.php
@@ -21,46 +21,13 @@ include('templates/header.php'); ?>
 <div class="top1_inner clearfix">
 <div class="top1_inner_bg"></div>
 <header><div class="logo_wrapper"><a href="index.php" class="logo"><img class="llogo" src="images/logo.png" alt=""></a></div></header>
-<div class="navbar navbar_">
-	<div class="navbar-inner navbar-inner_">
-		<a class="btn btn-navbar btn-navbar_" data-toggle="collapse" data-target=".nav-collapse_">
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-			<span class="icon-bar"></span>
-		</a>
-		<div class="nav-collapse nav-collapse_ collapse">
-			<ul class="nav sf-menu clearfix">
-      <li class="active"><a href="index.php">Home</a></li>
-      <li><a href="index-1.php">About Us</a></li>
-				<li class="sub-menu sub-menu-1"><a href="index-2.php">Products</a>
-					<ul>
-						<li><a href="404.php">404 page not found</a></li>
-						<li class="sub-menu sub-menu-2"><a href="index-2.php">specials</a>
-							<ul>
-								<li><a href="404.php">404 page not found</a></li>
-								<li><a href="index-2.php">sit amet</a></li>
-								<li><a href="index-2.php">adipiscing elit</a></li>
-								<li><a href="index-2.php">nunc suscipit</a></li>
-								<li><a href="404.php">404 page not found</a></li>
-							</ul>
-						</li>
-						<li class="sub-menu sub-menu-2"><a href="index-2.php">price list</a>
-							<ul>
-								<li><a href="index-2.php">lorem ipsum dolor</a></li>
-								<li><a href="index-2.php">sit amet</a></li>
-								<li><a href="index-2.php">adipiscing elit</a></li>
-								<li><a href="index-2.php">nunc suscipit</a></li>
-							</ul>
-						</li>
-					</ul>
-				</li>
-				<li><a href="index-3.php">Services</a></li>
-				<li><a href="index-4.php">Projects</a></li>
-				<li><a href="index-5.php">Contacts</a></li>											
-    </ul>
-		</div>
-	</div>
-</div>
+<?php 
+$index1="true";
+$index2="false";
+$index3="false";
+$index4="false";
+$index5="false";
+include('templates/navbar.php'); ?>
 </div>	
 </div>	
 </div>	
