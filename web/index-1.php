ab7ce8691eb0c8699673325ad0f29bf335c8b099
Salvatore <salvatore@byui.edu>
2019-10-12 08:42:37 -0300

    404

--- web/index-1.php
+++ web/index-1.php
@@ -4,6 +4,12 @@
 <div id="main">
 
 <div class="top1">
+<div class="container">
+<div class="row">
+<div class="span12">
+<div class="top1_inner clearfix">
+<div class="top1_inner_bg"></div>
+<header><div class="logo_wrapper"><a href="index.html" class="logo"><img class="llogo" src="images/logo.png" alt=""></a></div></header>
 
 <?php 
 $index1="true";
@@ -11,8 +17,7 @@ $index2="false";
 $index3="false";
 $index4="false";
 $index5="false";
-include('templates/navbar.php'); 
-?>
+include('templates/navbar.php'); ?>
 
 
 <div class="breadcrumbs1">
