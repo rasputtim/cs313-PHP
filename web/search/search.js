8e18db3b68c597378efb728ec44275bb39e00554
Salvatore <salvatore@byui.edu>
2019-10-13 17:52:45 -0300

    database access

--- web/search/search.js
+++ web/search/search.js
@@ -1,12 +1,12 @@
 $(function(){
 	var s=location.search.replace(/^\?.*s=([^&]+)/,'$1')
-	    ,t=location.search.replace(/^\?.*s=([^&]+)/,'$1')
+	    ,t=location.search.replace(/^\?.*t=([^&]+)/,'$1')
 		,form=$('#search-form')
 		,input=$('input[type=text]',form)
 		,results=$('#search-results').height(0)
 		,src='search/results.php'
 		,ifr=$('<iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" allowTransparency="true"></iframe>')
-		window.alert(s);
+		window.alert(t);
 	if(results.length)		
 		ifr		
 			.attr({
