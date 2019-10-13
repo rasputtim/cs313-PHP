1f373f001c8e1bb7b943deedd128b8b3ece3c849
Salvatore <salvatore@byui.edu>
2019-10-13 18:18:26 -0300

    database access

--- web/search/results.php
+++ web/search/results.php
@@ -15,8 +15,8 @@ $search_term_length = strlen($search_term);
 $final_result = array();
 
 $count = 0;
-$stmt = $db->prepare("SELECT count(*) FROM :tb WHERE catname = :op");
-$stmt->bindValue(':tb', $search_table, PDO::PARAM_STR);
+$stmt = $db->prepare("SELECT count(*) FROM $search_table WHERE catname = :op");
+//$stmt->bindValue(':tb', $search_table, PDO::PARAM_STR);
 $stmt->bindValue(':op', $search_term, PDO::PARAM_STR);
 $stmt->execute();
 $count = $stmt->fetchColumn();
