5efe58ab523cb5d6cb795c3b953a7d448672c97e
Salvatore <salvatore@byui.edu>
2019-10-13 17:34:13 -0300

    database access

--- web/index-2.php
+++ web/index-2.php
@@ -37,6 +37,7 @@ include('templates/menubar.php');
 <div class="row">
 		<div class="search-form-wrapper clearfix">
 			<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="navbar-form clearfix" >
+				<input type="hidden" id="seach_what" name="searchwhat" value="category">
 				<input type="text" name="s" value='Search' onBlur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''">
 				<a href="#" onClick="document.getElementById('search-form').submit()"></a>
 			</form>
