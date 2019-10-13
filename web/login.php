4056fcd154f2e624fc272a17cc0a424f88a05737
Salvatore <salvatore@byui.edu>
2019-10-13 12:18:35 -0300

    database access

--- web/login.php
+++ web/login.php
@@ -80,6 +80,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
                         } else{
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
+                            header("location: inc/noaccess.php");
                         }
                   
                 } else{
