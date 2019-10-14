74a61b68134b756a82d15bad66bf133bcfd57a0d
Salvatore <salvatore@byui.edu>
2019-10-14 19:56:43 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -207,46 +207,114 @@ CREATE TABLE ezfin_category(
 </div>
 <script type="text/javascript" src="js/bootstrap.js"></script>
 <script>
-	$(document).ready(function() {	
-		//	carouFredSel
-		$('#slider4 .carousel.main ul').carouFredSel({
-			auto: {
-				timeoutDuration: 8000					
-			},
-			responsive: true,
-			prev: '.prev4',
-			next: '.next4',
-			width: '100%',
-			scroll: {
-				items: 1,
-				duration: 1000,
-				easing: "easeOutExpo"
-			},			
-			items: {
-				width: '350',
-				height: 'variable',	//	optionally resize item-height			  
-				visible: {
-					min: 1,
-					max: 4
-				}
-			},
-			mousewheel: false,
-			swipe: {
-				onMouse: true,
-				onTouch: true
-				}
-		});
-		$(window).bind("resize",updateSizes_vat).bind("load",updateSizes_vat);
-		function updateSizes_vat(){		
-			$('#slider4 .carousel.main ul').trigger("updateSizes");
-		}
-		updateSizes_vat();
-	
-	
-	
-	
-	
-	}); //
+	$(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+        // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+        feedbackIcons: {
+            valid: 'glyphicon glyphicon-ok',
+            invalid: 'glyphicon glyphicon-remove',
+            validating: 'glyphicon glyphicon-refresh'
+        },
+        fields: {
+            first_name: {
+                validators: {
+                        stringLength: {
+                        min: 2,
+                    },
+                        notEmpty: {
+                        message: 'Please enter your First Name'
+                    }
+                }
+            },
+             last_name: {
+                validators: {
+                     stringLength: {
+                        min: 2,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Last Name'
+                    }
+                }
+            },
+			 user_name: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Username'
+                    }
+                }
+            },
+			 user_password: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please enter your Password'
+                    }
+                }
+            },
+			confirm_password: {
+                validators: {
+                     stringLength: {
+                        min: 8,
+                    },
+                    notEmpty: {
+                        message: 'Please confirm your Password'
+                    }
+                }
+            },
+            email: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please enter your Email Address'
+                    },
+                    emailAddress: {
+                        message: 'Please enter a valid Email Address'
+                    }
+                }
+            },
+            contact_no: {
+                validators: {
+                  stringLength: {
+                        min: 12, 
+                        max: 12,
+                    notEmpty: {
+                        message: 'Please enter your Contact No.'
+                     }
+                }
+            },
+			 department: {
+                validators: {
+                    notEmpty: {
+                        message: 'Please select your Department/Office'
+                    }
+                }
+            },
+                }
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow") // Do something ...
+                $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});
 	$(window).load(function() {
 		//
 	
