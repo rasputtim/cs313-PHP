9da66a473384df982b25eecf3b6d4e5138547a75
Salvatore <salvatore@byui.edu>
2019-10-14 20:57:42 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -59,57 +59,60 @@ CREATE TABLE ezfin_category(
 -->
 <form class="needs-validation" novalidate>
   <div class="form-row">
-    <div class="col-md-4 mb-3 md-form">
-      <label for="validationCustom012">First name</label>
-      <input type="text" class="form-control" id="validationCustom012" placeholder="First name" value="Mark"
-        required>
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom01">First name</label>
+      <input type="text" class="form-control" id="validationCustom01" placeholder="First name" value="Mark" required>
       <div class="valid-feedback">
         Looks good!
       </div>
     </div>
-    <div class="col-md-4 mb-3 md-form">
-      <label for="validationCustom022">Last name</label>
-      <input type="text" class="form-control" id="validationCustom022" value="Otto" required>
+    <div class="col-md-4 mb-3">
+      <label for="validationCustom02">Last name</label>
+      <input type="text" class="form-control" id="validationCustom02" placeholder="Last name" value="Otto" required>
       <div class="valid-feedback">
         Looks good!
       </div>
     </div>
-    <div class="col-md-4 mb-3 md-form">
-      <label for="validationCustomUsername2">Username</label>
-      <input type="text" class="form-control" id="validationCustomUsername2" aria-describedby="inputGroupPrepend2"
-        required>
-      <div class="invalid-feedback">
-        Please choose a username.
+    <div class="col-md-4 mb-3">
+      <label for="validationCustomUsername">Username</label>
+      <div class="input-group">
+        <div class="input-group-prepend">
+          <span class="input-group-text" id="inputGroupPrepend">@</span>
+        </div>
+        <input type="text" class="form-control" id="validationCustomUsername" placeholder="Username" aria-describedby="inputGroupPrepend" required>
+        <div class="invalid-feedback">
+          Please choose a username.
+        </div>
       </div>
     </div>
   </div>
   <div class="form-row">
-    <div class="col-md-6 mb-3 md-form">
-      <label for="validationCustom032">City</label>
-      <input type="text" class="form-control" id="validationCustom032" required>
+    <div class="col-md-6 mb-3">
+      <label for="validationCustom03">City</label>
+      <input type="text" class="form-control" id="validationCustom03" placeholder="City" required>
       <div class="invalid-feedback">
         Please provide a valid city.
       </div>
     </div>
-    <div class="col-md-3 mb-3 md-form">
-      <label for="validationCustom042">State</label>
-      <input type="text" class="form-control" id="validationCustom042" required>
+    <div class="col-md-3 mb-3">
+      <label for="validationCustom04">State</label>
+      <input type="text" class="form-control" id="validationCustom04" placeholder="State" required>
       <div class="invalid-feedback">
         Please provide a valid state.
       </div>
     </div>
-    <div class="col-md-3 mb-3 md-form">
-      <label for="validationCustom052">Zip</label>
-      <input type="text" class="form-control" id="validationCustom052" required>
+    <div class="col-md-3 mb-3">
+      <label for="validationCustom05">Zip</label>
+      <input type="text" class="form-control" id="validationCustom05" placeholder="Zip" required>
       <div class="invalid-feedback">
         Please provide a valid zip.
       </div>
     </div>
   </div>
   <div class="form-group">
-    <div class="form-check pl-0">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-      <label class="form-check-label" for="invalidCheck2">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="invalidCheck" required>
+      <label class="form-check-label" for="invalidCheck">
         Agree to terms and conditions
       </label>
       <div class="invalid-feedback">
@@ -117,7 +120,7 @@ CREATE TABLE ezfin_category(
       </div>
     </div>
   </div>
-  <button class="btn btn-primary btn-sm btn-rounded" type="submit">Submit form</button>
+  <button class="btn btn-primary" type="submit">Submit form</button>
 </form>
 
 
@@ -175,30 +178,23 @@ CREATE TABLE ezfin_category(
 </div>
 <script type="text/javascript" src="js/bootstrap.js"></script>
 <script>
-    (function() {
-'use strict';
-window.addEventListener('load', function() {
-// Fetch all the forms we want to apply custom Bootstrap validation styles to
-var forms = document.getElementsByClassName('needs-validation');
-// Loop over them and prevent submission
-var validation = Array.prototype.filter.call(forms, function(form) {
-form.addEventListener('submit', function(event) {
-if (form.checkValidity() === false) {
-event.preventDefault();
-event.stopPropagation();
-}
-form.classList.add('was-validated');
-}, false);
-});
-}, false);
-})();
-	$(document).ready(function() {
-        $('#myFormGallery').formgallery(options);
+(function() {
+  'use strict';
+  window.addEventListener('load', function() {
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.getElementsByClassName('needs-validation');
+    // Loop over them and prevent submission
+    var validation = Array.prototype.filter.call(forms, function(form) {
+      form.addEventListener('submit', function(event) {
+        if (form.checkValidity() === false) {
+          event.preventDefault();
+          event.stopPropagation();
+        }
+        form.classList.add('was-validated');
+      }, false);
     });
-	$(window).load(function() {
-		//
-	
-	}); //
-	</script>	
+  }, false);
+})();
+</script>	
 </body>
 </html>
