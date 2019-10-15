985713ca749fc0c32009d9d1b4def598ecdf512d
Salvatore <salvatore@byui.edu>
2019-10-14 22:14:30 -0300

    added

--- web/templates/menubar.php
+++ web/templates/menubar.php
@@ -2,7 +2,7 @@
 <div class="row">
 <div class="span12">
 <div class="top1_inner clearfix">
-
+<div class="top1_inner_bg"></div>
 <header><div class="logo_wrapper"><a href="index.php" class="logo"><img class="llogo" src="images/logo.png" alt=""></a></div></header>
 <?php
 
