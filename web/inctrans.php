7c3815e15904f7b4267e33aa3e297284b9408c2b
Salvatore <salvatore@byui.edu>
2019-10-14 23:46:42 -0300

    added

--- web/inctrans.php
+++ web/inctrans.php
@@ -190,6 +190,9 @@ CREATE TABLE ezfin_transactions (
 	
 </div>
 <script type="text/javascript" src="js/bootstrap.js"></script>
+<script src="js/jquery.min.js"></script>
+    <script src="js/jquery-ui.min.js"></script>
+    <script src="js/main.js"></script>
 <script>
 	$(document).ready(function() {	
 		//
