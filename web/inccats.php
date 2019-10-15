41eb686cf5ddf04c91cc8956ce79d098b60cc946
Salvatore <salvatore@byui.edu>
2019-10-15 18:45:16 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -114,7 +114,7 @@ if ($is_insert){ // Create group
                                 
                                 
                             </div>
-                            <div class="col-sm-6">
+                            <div class="col-sm-6 checkbox">
 
 								<label for="operation"> Operation:</label>
 								<div class="col-sm-6 ">
