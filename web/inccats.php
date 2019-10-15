336aeec3b0be14d265c2765a60b8527599bf67d5
Salvatore <salvatore@byui.edu>
2019-10-14 21:20:34 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -57,10 +57,7 @@ CREATE TABLE ezfin_category(
             PRIMARY KEY ( idCat, idUser)
             );
 -->
-
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-md-offset-3" id="form_container">
+<div class="col-md-6 col-md-offset-3" id="form_container">
                     <h2>Contact Us</h2> 
                     <p> Please send your message below. We will get back to you at the earliest! </p>
                     <form role="form" method="post" id="reused_form">
@@ -88,9 +85,7 @@ CREATE TABLE ezfin_category(
                     </form>
                     <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Posted your message successfully!</h3> </div>
                     <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
-                </div>
-            </div>
-        </div>
+</div>
 
 
 
