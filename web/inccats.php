4fe19be855a8fd794ab5bb675daf769872de3463
Salvatore <salvatore@byui.edu>
2019-10-14 22:20:28 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -57,44 +57,7 @@ CREATE TABLE ezfin_category(
             PRIMARY KEY ( idCat, idUser)
             );
 -->
-<div class="col-md-6 col-md-offset-3" id="form_container">
-                    <h2>CATEGORY FORM</h2> 
-                    <p> Description </p>
-                    <form role="form" method="post" id="reused_form">
-                        
-                        <div class="row">
-                            <div class="col-sm-6 form-group">
-                                <label for="name"> Category Name:</label>
-                                <input type="text" class="form-control" id="name" name="name" required>
-                            </div>
-                            <div class="col-sm-6 form-group">
-                                <label for="name"> Category Alias:</label>
-                                <input type="text" class="form-control" id="name" name="name" required>
-                            </div>
-                            <div class="col-sm-6 form-group">
-                                <label for="email"> Email:</label>
-                                <input type="email" class="form-control" id="email" name="email" required>
-                            </div>
-                        </div>
 
-                        
-                        <div class="row">
-                            <div class="col-sm-12 form-group">
-                                <label for="message"> Description:</label>
-                                <textarea class="form-control" type="textarea" name="message" id="message" maxlength="6000" rows="3"></textarea>
-                            </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-sm-12 form-group">
-                                <button type="submit" class="btn btn-lg btn-default pull-left" >Send &rarr;</button>
-                            </div>
-                        </div>
-                    </form>
-                    <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Posted your message successfully!</h3> </div>
-                    <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
-
-
-</div>
 <div class="span3">
 
 <h2>category List</h2>
