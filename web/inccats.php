7e6eb9fecff60b2132e633635a73cc24bac8f227
Salvatore <salvatore@byui.edu>
2019-10-14 21:01:19 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -180,6 +180,7 @@ CREATE TABLE ezfin_category(
 <script>
 (function() {
   'use strict';
+  window.alert("ENTROU");
   window.addEventListener('load', function() {
     // Fetch all the forms we want to apply custom Bootstrap validation styles to
     var forms = document.getElementsByClassName('needs-validation');
