8e23fdc80df88d2744563369fc3c6d3317e62e30
Salvatore <salvatore@byui.edu>
2019-10-15 12:58:11 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -10,50 +10,61 @@ require_once ("inc/connect.php");
 
 
 
+
+    
 /**
- * Convert the $value encode in html entity to clear char string. This function
- * should be called always to "clean" HTML encoded data; to render to a text
- * plain ascii file, to render to console, or to put in any kind of data field
- * who doesn't make the HTML render by itself.
+ * Cleans a string by encoding to UTF-8 and replacing the HTML
+ * entities. UTF-8 is necessary for foreign chars like asian
+ * and our databases are (or should be) UTF-8
  *
  * @param mixed String or array of strings to be cleaned.
- * @param boolean $utf8 Flag, set the output encoding in utf8, by default true.
  *
- * @return unknown_type
+ * @return mixed The cleaned string or array.
  */
-function safe_output($value, $utf8 = true)
-{
+function safe_input($value) {
+	//Stop!! Are you sure to modify this critical code? Because the older
+	//versions are serius headache in many places of Pandora.
+
 	if (is_numeric($value))
 		return $value;
 
 	if (is_array($value)) {
-		array_walk($value, "safe_output");
+		array_walk($value, "safe_input_array");
 		return $value;
 	}
-    $valueHtmlEncode = htmlspecialchars($value);
+
+	//Clean the trash mix into string because of magic quotes.
+	//if (get_magic_quotes_gpc() == 1) {
+	//	$value = stripslashes($value);
+	//}
+
 	//if (! mb_check_encoding ($value, 'UTF-8'))
 	//	$value = utf8_encode ($value);
 
-	//if ($utf8) {
-	//	$valueHtmlEncode =  html_entity_decode ($value, ENT_QUOTES, "UTF-8");
-	//}
-	//else {
-	//	$valueHtmlEncode =  html_entity_decode ($value, ENT_QUOTES);
-	//}
+	$valueHtmlEncode =  htmlentities ($value, ENT_QUOTES, "UTF-8");
 
-	//Replace the html entitie of ( for the char
-	$valueHtmlEncode = str_replace("&#40;", '(', $valueHtmlEncode);
+	//Replace the character '\' for the equivalent html entitie
+	$valueHtmlEncode = str_replace('\\', "&#92;", $valueHtmlEncode);
 
-	//Replace the html entitie of ) for the char
-	$valueHtmlEncode = str_replace("&#41;", ')', $valueHtmlEncode);
+	// First attempt to avoid SQL Injection based on SQL comments
+	// Specific for MySQL.
+	$valueHtmlEncode = str_replace('/*', "&#47;&#42;", $valueHtmlEncode);
+	$valueHtmlEncode = str_replace('*/', "&#42;&#47;", $valueHtmlEncode);
 
-	//Revert html entities to chars
-	//for ($i=0;$i<33;$i++) {
-	//	$valueHtmlEncode = str_ireplace("&#x".dechex($i).";",html_to_ascii(dechex($i)), $valueHtmlEncode);
-	//}
-    //
+	//Replace ( for the html entitie
+	$valueHtmlEncode = str_replace('(', "&#40;", $valueHtmlEncode);
+
+	//Replace ( for the html entitie
+	$valueHtmlEncode = str_replace(')', "&#41;", $valueHtmlEncode);
+
+	//Replace some characteres for html entities
+	for ($i=0;$i<33;$i++) {
+		$valueHtmlEncode = str_ireplace(chr($i),ascii_to_html($i), $valueHtmlEncode);
+	}
+
 	return $valueHtmlEncode;
 }
+
 
 /**
  * Get a parameter from get request array.
