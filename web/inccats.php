700a7c3eee0a52921c848b9fe2b1063a4bd02e5c
Salvatore <salvatore@byui.edu>
2019-10-14 22:18:02 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -77,6 +77,20 @@ CREATE TABLE ezfin_category(
                             </div>
                         </div>
                         <div class="row">
+                        <div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1">
+  <label class="form-check-label" for="inlineRadio1">1</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2">
+  <label class="form-check-label" for="inlineRadio2">2</label>
+</div>
+<div class="form-check form-check-inline">
+  <input class="form-check-input" type="radio" name="inlineRadioOptions" id="inlineRadio3" value="option3" disabled>
+  <label class="form-check-label" for="inlineRadio3">3 (disabled)</label>
+</div>
+</div>
+                        <div class="row">
                             <div class="col-sm-12 form-group">
                                 <label for="message"> Description:</label>
                                 <textarea class="form-control" type="textarea" name="message" id="message" maxlength="6000" rows="3"></textarea>
@@ -90,72 +104,6 @@ CREATE TABLE ezfin_category(
                     </form>
                     <div id="success_message" style="width:100%; height:100%; display:none; "> <h3>Posted your message successfully!</h3> </div>
                     <div id="error_message" style="width:100%; height:100%; display:none; "> <h3>Error</h3> Sorry there was an error sending your form. </div>
-</div>
-<form class="needs-validation" novalidate>
-  <div class="form-row">
-    <div class="col-md-4 mb-3 md-form">
-      <label for="validationCustom012">First name</label>
-      <input type="text" class="form-control" id="validationCustom012" placeholder="First name" value="Mark"
-        required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
-    </div>
-    <div class="col-md-4 mb-3 md-form">
-      <label for="validationCustom022">Last name</label>
-      <input type="text" class="form-control" id="validationCustom022" value="Otto" required>
-      <div class="valid-feedback">
-        Looks good!
-      </div>
-    </div>
-    <div class="col-md-4 mb-3 md-form">
-      <label for="validationCustomUsername2">Username</label>
-      <input type="text" class="form-control" id="validationCustomUsername2" aria-describedby="inputGroupPrepend2"
-        required>
-      <div class="invalid-feedback">
-        Please choose a username.
-      </div>
-    </div>
-  </div>
-  <div class="form-row">
-    <div class="col-md-6 mb-3 md-form">
-      <label for="validationCustom032">City</label>
-      <input type="text" class="form-control" id="validationCustom032" required>
-      <div class="invalid-feedback">
-        Please provide a valid city.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3 md-form">
-      <label for="validationCustom042">State</label>
-      <input type="text" class="form-control" id="validationCustom042" required>
-      <div class="invalid-feedback">
-        Please provide a valid state.
-      </div>
-    </div>
-    <div class="col-md-3 mb-3 md-form">
-      <label for="validationCustom052">Zip</label>
-      <input type="text" class="form-control" id="validationCustom052" required>
-      <div class="invalid-feedback">
-        Please provide a valid zip.
-      </div>
-    </div>
-  </div>
-  <div class="form-group">
-    <div class="form-check pl-0">
-      <input class="form-check-input" type="checkbox" value="" id="invalidCheck2" required>
-      <label class="form-check-label" for="invalidCheck2">
-        Agree to terms and conditions
-      </label>
-      <div class="invalid-feedback">
-        You must agree before submitting.
-      </div>
-    </div>
-  </div>
-  <button class="btn btn-primary btn-sm btn-rounded" type="submit">Submit form</button>
-</form>
-
-
-
 
 
 </div>
