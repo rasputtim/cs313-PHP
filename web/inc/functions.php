9dc454175ada85b2e21bb63f2c52f39abc4c07e8
Salvatore <salvatore@byui.edu>
2019-10-15 18:14:53 -0300

    added

--- web/inc/functions.php
+++ web/inc/functions.php
@@ -23,6 +23,61 @@ return empty($eregi) ? true : false;
 }
 
 /**
+ * Cleans a string by encoding to UTF-8 and replacing the HTML
+ * entities. UTF-8 is necessary for foreign chars like asian
+ * and our databases are (or should be) UTF-8
+ *
+ * @param mixed String or array of strings to be cleaned.
+ *
+ * @return mixed The cleaned string or array.
+ */
+function safe_input($value) {
+	//Stop!! Are you sure to modify this critical code? Because the older
+	//versions are serius headache in many places.
+
+	if (is_numeric($value))
+		return $value;
+
+	if (is_array($value)) {
+		array_walk($value, "safe_input_array");
+		return $value;
+	}
+
+
+	//Clean the trash mix into string because of magic quotes.
+	//if (get_magic_quotes_gpc() == 1) {
+		$value = stripslashes($value);
+	//}
+
+	//if (! mb_check_encoding ($value, 'UTF-8'))
+		$value = utf8_encode ($value);
+
+	$valueHtmlEncode =  htmlentities ($value);
+	//$valueHtmlEncode =  htmlspecialchars($value); //
+	//Replace the character '\' for the equivalent html entitie
+	$valueHtmlEncode = str_replace('\\', "&#92;", $valueHtmlEncode);
+
+	// First attempt to avoid SQL Injection based on SQL comments
+	// Specific for MySQL.
+	$valueHtmlEncode = str_replace('/*', "&#47;&#42;", $valueHtmlEncode);
+	$valueHtmlEncode = str_replace('*/', "&#42;&#47;", $valueHtmlEncode);
+
+	//Replace ( for the html entitie
+	$valueHtmlEncode = str_replace('(', "&#40;", $valueHtmlEncode);
+
+	//Replace ( for the html entitie
+	$valueHtmlEncode = str_replace(')', "&#41;", $valueHtmlEncode);
+
+	//Replace some characteres for html entities
+	//for ($i=0;$i<33;$i++) {
+	//	$valueHtmlEncode = str_ireplace(chr($i),ascii_to_html($i), $valueHtmlEncode);
+	//}
+
+	return $valueHtmlEncode;
+}
+
+
+/**
  * Get a parameter from get request array.
  *
  * @param name Name of the parameter
@@ -32,7 +87,7 @@ return empty($eregi) ? true : false;
  */
 function get_parameter_get ($name, $default = "") {
 	if ((isset ($_GET[$name])) && ($_GET[$name] != ""))
-		return safe_input ($_GET[$name]);
+		return safe_input($_GET[$name]);
 
 	return $default;
 }
@@ -47,7 +102,7 @@ function get_parameter_get ($name, $default = "") {
  */
 function get_parameter_post ($name, $default = "") {
 	if ((isset ($_POST[$name])) && ($_POST[$name] != ""))
-		return safe_input ($_POST[$name]);
+		return safe_input($_POST[$name]);
 
 	return $default;
 }
@@ -64,6 +119,8 @@ function get_parameter_post ($name, $default = "") {
  * @return
  */
 function get_parameter ($name, $default = '') {
+
+	
 	// POST has precedence
 	if (isset($_POST[$name]))
 		return get_parameter_post ($name, $default);
@@ -73,6 +130,7 @@ function get_parameter ($name, $default = '') {
 
 	return $default;
 }
+
 
 
 ?>
