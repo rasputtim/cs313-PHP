a0b1ff3f5adf9af54ec99405a1d20dd385b59738
Salvatore <salvatore@byui.edu>
2019-10-15 18:17:29 -0300

    added

--- web/inccats.php
+++ web/inccats.php
@@ -6,120 +6,12 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     //exit;
 }
 require_once ("inc/connect.php");
-//require_once ("inc/functions.php");
+require_once ("inc/functions.php");
 
 
 
 
     
-/**
- * Cleans a string by encoding to UTF-8 and replacing the HTML
- * entities. UTF-8 is necessary for foreign chars like asian
- * and our databases are (or should be) UTF-8
- *
- * @param mixed String or array of strings to be cleaned.
- *
- * @return mixed The cleaned string or array.
- */
-function safe_input($value) {
-	//Stop!! Are you sure to modify this critical code? Because the older
-	//versions are serius headache in many places.
-
-	if (is_numeric($value))
-		return $value;
-
-	if (is_array($value)) {
-		array_walk($value, "safe_input_array");
-		return $value;
-	}
-
-
-	//Clean the trash mix into string because of magic quotes.
-	//if (get_magic_quotes_gpc() == 1) {
-		$value = stripslashes($value);
-	//}
-
-	//if (! mb_check_encoding ($value, 'UTF-8'))
-		$value = utf8_encode ($value);
-
-	$valueHtmlEncode =  htmlentities ($value);
-	//$valueHtmlEncode =  htmlspecialchars($value); //
-	//Replace the character '\' for the equivalent html entitie
-	$valueHtmlEncode = str_replace('\\', "&#92;", $valueHtmlEncode);
-
-	// First attempt to avoid SQL Injection based on SQL comments
-	// Specific for MySQL.
-	$valueHtmlEncode = str_replace('/*', "&#47;&#42;", $valueHtmlEncode);
-	$valueHtmlEncode = str_replace('*/', "&#42;&#47;", $valueHtmlEncode);
-
-	//Replace ( for the html entitie
-	$valueHtmlEncode = str_replace('(', "&#40;", $valueHtmlEncode);
-
-	//Replace ( for the html entitie
-	$valueHtmlEncode = str_replace(')', "&#41;", $valueHtmlEncode);
-
-	//Replace some characteres for html entities
-	//for ($i=0;$i<33;$i++) {
-	//	$valueHtmlEncode = str_ireplace(chr($i),ascii_to_html($i), $valueHtmlEncode);
-	//}
-
-	return $valueHtmlEncode;
-}
-
-
-/**
- * Get a parameter from get request array.
- *
- * @param name Name of the parameter
- * @param default Value returned if there were no parameter.
- *
- * @return Parameter value.
- */
-function get_parameter_get ($name, $default = "") {
-	if ((isset ($_GET[$name])) && ($_GET[$name] != ""))
-		return safe_input($_GET[$name]);
-
-	return $default;
-}
-
-/**
- * Get a parameter from post request array.
- *
- * @param name Name of the parameter
- * @param default Value returned if there were no parameter.
- *
- * @return Parameter value.
- */
-function get_parameter_post ($name, $default = "") {
-	if ((isset ($_POST[$name])) && ($_POST[$name] != ""))
-		return safe_input($_POST[$name]);
-
-	return $default;
-}
-
-/**
- * Get a paramter from a request.
- *
- * It checks first on post request, if there were nothing defined, it
- * would return get request
- *
- * @param name
- * @param default
- *
- * @return
- */
-function get_parameter ($name, $default = '') {
-
-	
-	// POST has precedence
-	if (isset($_POST[$name]))
-		return get_parameter_post ($name, $default);
-
-	if (isset($_GET[$name]))
-		return get_parameter_get ($name, $default);
-
-	return $default;
-}
 
 
 $is_insert = get_parameter("create2");
@@ -134,6 +26,7 @@ if ($is_insert){ // Create group
 	$my_name = get_parameter("name");
 	$my_alias = get_parameter("alias");
 	$my_icon = get_parameter("icon");
+	$my_oper = get_parameter("operation");
     $my_description = htmlspecialchars($_POST['descript']) ;//get_paramenter("descript");
 
 }
@@ -186,6 +79,7 @@ if ($is_insert){ // Create group
 	echo "<h3 >$my_name</h3>";
 	echo "<h3 >$my_alias</h3>";
 	echo "<h3 >$my_icon</h3>";
+	echo "<h3 >$my_oper</h3>";
     echo "<h3 >$my_description</h3>";
 
 }
