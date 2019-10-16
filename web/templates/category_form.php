8a4acd841658fba0ce3a0d7d9171e1098d2aff47
Salvatore <salvatore@byui.edu>
2019-10-16 11:04:04 -0300

    addcat

--- web/templates/category_form.php
+++ web/templates/category_form.php
@@ -98,20 +98,20 @@
 									
 							
 							</div>
-							<div class="row">
-							<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="operation" id="inlineRadio1" value="0" checked="<?php echo $my_checked_income;?>">
-								<label class="form-check-label" for="inlineRadio1">INCOME</label>
+							
+							    <div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="operation" id="inlineRadio1" value="0" checked="<?php echo $my_checked_income;?>">
+                                    <label class="form-check-label" for="inlineRadio1">INCOME</label>
+                                </div>
+								<div class="form-check form-check-inline">
+                                    <input class="form-check-input" type="radio" name="operation" id="inlineRadio2" value="1" checked="<?php echo $my_checked_outcome;?>">
+                                    <label class="form-check-label" for="inlineRadio2">OUTCOME</label>
 								</div>
 								<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="operation" id="inlineRadio2" value="1" checked="<?php echo $my_checked_outcome;?>">
-								<label class="form-check-label" for="inlineRadio2">OUTCOME</label>
+                                    <input class="form-check-input" type="radio" name="operation" id="inlineRadio3" value="2" checked="<?php echo $my_checked_informative;?>">
+                                    <label class="form-check-label" for="inlineRadio3">INFORMATIVE</label>
 								</div>
-								<div class="form-check form-check-inline">
-								<input class="form-check-input" type="radio" name="operation" id="inlineRadio3" value="2" checked="<?php echo $my_checked_informative;?>">
-								<label class="form-check-label" for="inlineRadio3">INFORMATIVE</label>
-								</div>
-							</div>	
+							
 							<div class="row">
 								<div class="col-sm-12 form-group">
 									<label for="descript"> Description:</label>
