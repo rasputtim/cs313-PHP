cda92fe72035d79f44668055d3749d99ad4284e4
Salvatore <salvatore@byui.edu>
2019-10-16 09:07:57 -0300

    addcat

--- web/templates/search.php
+++ web/templates/search.php
@@ -1,9 +1,9 @@
 <?php
 echo'<div class="search-form-wrapper clearfix">';
 echo'<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="my-form clearfix" >';
-echo'<input type="hidden" id="seach_what" name="t" value="category">';
-echo'<input type="text" name="s" value="Search" onBlur="if(this.value=="") this.value="Search" onFocus="if(this.value =="Search" ) this.value=""">';
-				echo'<a href="#" onClick="document.getElementById("search-form").submit()"></a>';
+echo'<input type="hidden" id="seach_what" name="t" value="category">'; ?>
+				<input type="text" name="s" value='Search' onBlur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''">
+	<?php			echo'<a href="#" onClick="document.getElementById("search-form").submit()"></a>';
                 echo'</form>';
                 echo'</div> ';
 ?>
