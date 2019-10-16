eed5d9c2a97d49fbfb959f21b10e7aeafdbf5de9
Salvatore <salvatore@byui.edu>
2019-10-16 09:14:01 -0300

    addcat

--- web/listtrans.php
+++ web/listtrans.php
@@ -39,14 +39,11 @@ include('templates/menubar.php');
 <div id="content">
 <div class="container">
 <div class="row">
-
-		<div class="search-form-wrapper clearfix">
-			<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="navbar-form clearfix" >
-				<input type="hidden" id="seach_what" name="t" value="transactions">
-				<input type="text" name="s" value='Search' onBlur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''">
-				<a href="#" onClick="document.getElementById('search-form').submit()"></a>
-			</form>
-		</div>
+		<?php
+		$search_type = "transactions";
+			include('templates/search.php'); 
+		?>
+		
 
 <div class="col-lg-9">
 	
