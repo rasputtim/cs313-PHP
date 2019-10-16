764f402db99ecdbafcf929b7f69ad65a93377753
Salvatore <salvatore@byui.edu>
2019-10-16 08:46:40 -0300

    addcat

--- web/listcats.php
+++ web/listcats.php
@@ -37,7 +37,7 @@ include('templates/menubar.php');
 <div class="container">
 <div class="row">
 		<div class="search-form-wrapper clearfix">
-			<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="clearfix" >
+			<form id="search-form" action="search.php" method="GET" accept-charset="utf-8" class="my-form clearfix" >
 				<input type="hidden" id="seach_what" name="t" value="category">
 				<input type="text" name="s" value='Search' onBlur="if(this.value=='') this.value='Search'" onFocus="if(this.value =='Search' ) this.value=''">
 				<a href="#" onClick="document.getElementById('search-form').submit()"></a>
