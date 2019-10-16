d746524a461eaa0b17b33c30ee29592737a5596f
Salvatore <salvatore@byui.edu>
2019-10-16 13:35:20 -0300

    addcat

--- web/listcats.php
+++ web/listcats.php
@@ -5,9 +5,11 @@ if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     //header("location:inc/noaccess.php");
     //exit;
 }
-require_once ("inc/connection.php");
 require_once ("inc/functions_db.php");
-include('templates/header.php'); ?>
+include('templates/header.php'); 
+$money_format = '%(#10n';
+$date_format = "D, M d, Y ";
+?>
 
 <body class="subpage">
 <div id="main">
@@ -15,19 +17,20 @@ include('templates/header.php'); ?>
 <div class="top1">
 <?php 
 $index1="false";
-$index2="true";
-$index3="false";
+$index2="false";
+$index3="true";
 $index4="false";
 $index5="false";
 include('templates/menubar.php'); 
 ?>
+
 </div>
 
 <div class="breadcrumbs1">
 <div class="container">
 <div class="row">
 <div class="col-lg-12">
-<div class="breadcrumbs1_inner"><a href="index.html">home page</a>&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;&nbsp;list categories</div>	
+<div class="breadcrumbs1_inner"><a href="index.html">home page</a>&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;&nbsp;transactions</div>	
 </div>	
 </div>	
 </div>	
@@ -37,227 +40,103 @@ include('templates/menubar.php');
 <div class="container">
 <div class="row">
 		<?php
-		$search_type = "category";
+		$search_type = "transactions";
 			include('templates/search.php'); 
 		?>
-</div>
+		
+
 <div class="col-lg-9">
 	
-<h1>categories  overview</h1>
+<h1>Transactions for the current period</h1>
 
 
 
 <?php
-$myOperation=1;
-$stmt = get_db()->prepare('SELECT * FROM public.ezfin_category WHERE operation=:op');
-$stmt->bindValue(':op', $myOperation, PDO::PARAM_INT);
+
+$stmt = get_db()->prepare('SELECT * FROM public.ezfin_transactions');
+//$stmt->bindValue(':op', $myOperation, PDO::PARAM_INT);
 $stmt->execute();
 $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
 $count =0;
 $added = false;
 foreach ($rows as $row)
 {
+	//get category
+	$oper_image ="cat_income_green_peq.png";
+	$stmt = get_db()->prepare('SELECT operation FROM public.ezfin_category WHERE idcat =  :op');
+	$stmt->bindValue(':op', $row['idcategory'], PDO::PARAM_INT);
+	$stmt->execute();
+	$operation = $stmt->fetchColumn();
+	switch($operation){
+	   case 0:
+	   $oper_image ="cat_income_green_peq.png";
+	   break;
+	   case 1:
+	   $oper_image ="cat_bill_red_peq.png";
+	   break;
+	   case 2:
+	   $oper_image = "cat_informative_peq.png";
+	   break;
+	}
 	$added = false;
-	if ($count % 3 == 0 ) echo '<div class="row">';
-	echo '<div class="col-lg-3">';
-	echo '<div class="thumb3">';
-	echo '<div class="thumbnail clearfix">';		
-	echo '<figure class=""><img src="images/'. $row['icon'] . '.png " alt=""></figure>';
-	echo '<div class="caption">';
-	echo '<h3>' . $row['name'] . '</h3>';
-	echo '<p>';
-				echo $row['description'];
-				echo '</p>';
+	if ($count  == 0 ) echo '<ul class="thumbnails thumbnails1">';
+	echo '<li>';
+		echo '<div class="thumbnail clearfix">';
+		    // todo: add category icon here
+			echo '<figure class="oper_icon"><img src="images/'.$oper_image.'" alt=""></figure>';
+			echo '<div class="caption">';											
+				echo '<h3>'.date_format(date_create($row['duedate']),$date_format)." - $ ". money_format($money_format, $row['amount']);
+				echo '</h3>';
 				echo '<p>';
-				echo '<strong>Operation:'.$myOperation.'</strong>';
+						echo $row['description']. '<a href=" edittransacton.php?idtrans='.$row['idtransaction'].'"><strong>  edit</strong></a>';
 				echo '</p>';
-				echo '<a href="editcat.php?cat='.$row['idcat'].'" class="button2">edit </a>';
-				echo '</div>';		
-				echo '</div>';
-				echo '</div>';
-				echo '</div>';
-				if ($count == 2 ) {
-					echo '</div>';
-					$count = 0;
-					$added = true;
-				}else $count++;
-			
+			echo '</div>';			
+		echo '</div>';
+	echo '</li>';
+	if ($count  == 1 ) {
+		echo '</ul>';
+		$count = 0;
+		$added = true;
+	}else $count ++;
 }
-if ($added == false) echo '</div>';
-?>	
-
-
+if ($added = false) echo '</ul>';
+?>
+				
 
 
 </div>
 <div class="col-lg-3">
 
-<h2>category List</h2>
+<h2>Transactions List</h2>
 
 	<ul class="ul1">
-	<?php 
-	foreach ($db->query('SELECT name,idcat,operation FROM public.ezfin_category') as $row)
+<?php 
+	foreach (get_db()->query('SELECT * FROM public.ezfin_transactions') as $row)
 	{
-	echo '<li><a href="inccats.php?update='.$row['idcat'].'">';
-	echo $row['name'];
+	echo '<li><a href="#">';
+	echo $row['idcategory']." - ".$row['duedate']." - ". money_format($money_format, $row['amount']);
 	echo '</a></li>';
 	}
 ?>
-	  	            		      	      			      
+	    	            		      	      			      
 	</ul>	
 
 	
 
 	
 </div>	
-</div>	
-</div>	
 </div>
-
-<div id="slider4_wrapper">
-<div class="container">
 <div class="row">
 <div class="col-lg-12">
-<div id="slider4">
-<div class="slider4-title">CATEGORIES MOST USED</div>
-<div class="slider4_wrapper2">
-<a class="prev4" href="#"></a>
-<a class="next4" href="#"></a>
-<div class="carousel-box row">
-	<div class="inner col-lg-12">			
-		<div class="carousel main">
-			<ul>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products07.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products08.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products09.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products10.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products07.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products08.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products09.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>
-				<li>
-					<div class="thumb-carousel2 banner1">
-						<div class="thumbnail clearfix">
-							<a href="#">
-								<figure>
-									<img src="images/products10.jpg" alt="">
-								</figure>
-								<div class="caption">
-									<div class="txt1">Lorem ipsum dolor</div>
-									<div class="txt2">dolor sit amet conse ctetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna.</div>								
-								</div>								
-							</a>								
-						</div>
-					</div>
-				</li>																								
-			</ul>
-		</div>
-	</div>
-</div>	
-</div>
-</div>	
-</div>	
-</div>	
-</div>	
-</div>
 
+<div class="line1"></div>
+
+
+
+</div>	
+</div>	
+</div>	
+</div>
 
 
 <div class="bot1">
@@ -265,7 +144,9 @@ if ($added == false) echo '</div>';
 <div class="row">
 <div class="col-lg-12">
 <div class="bot1_inner">
-<?php include('templates/footer.php'); ?>
+
+	<?php include('templates/footer.php'); ?>
+	
 </div>	
 </div>	
 </div>
@@ -282,41 +163,7 @@ if ($added == false) echo '</div>';
 <script type="text/javascript" src="js/bootstrap.js"></script>
 <script>
 	$(document).ready(function() {	
-		//	carouFredSel
-		$('#slider4 .carousel.main ul').carouFredSel({
-			auto: {
-				timeoutDuration: 8000					
-			},
-			responsive: true,
-			prev: '.prev4',
-			next: '.next4',
-			width: '100%',
-			scroll: {
-				items: 1,
-				duration: 1000,
-				easing: "easeOutExpo"
-			},			
-			items: {
-				width: '350',
-				height: 'variable',	//	optionally resize item-height			  
-				visible: {
-					min: 1,
-					max: 4
-				}
-			},
-			mousewheel: false,
-			swipe: {
-				onMouse: true,
-				onTouch: true
-				}
-		});
-		$(window).bind("resize",updateSizes_vat).bind("load",updateSizes_vat);
-		function updateSizes_vat(){		
-			$('#slider4 .carousel.main ul').trigger("updateSizes");
-		}
-		updateSizes_vat();
-	
-	
+		//
 	
 	
 	
