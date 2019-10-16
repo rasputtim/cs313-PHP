ef518c4f3d518a171e740dab8c85a7dc0fc51c90
Salvatore <salvatore@byui.edu>
2019-10-16 09:35:44 -0300

    addcat

--- web/templates/navbar.php
+++ web/templates/navbar.php
@@ -41,8 +41,8 @@ if ($index5=="true"){
 						<li><a href="options.php">Options</a></li>
 						<li class="sub-menu sub-menu-2"><a href="listcats.php">Categories</a>
 							<ul>
-								<li><a href="inccats.php">Include</a></li>
-								<li><a href="editcats.php">Edit</a></li>
+								<li><a href="inccats.php?create=true">Include</a></li>
+								<li><a href="inccats.php?update=10">Edit</a></li>
 								<li><a href="listcats.php">Search</a></li>
 								
 							</ul>
