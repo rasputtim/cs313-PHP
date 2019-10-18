0c5bbffdb311a36a02713ce3b6a9ea430259a1fc
Salvatore <salvatore@byui.edu>
2019-10-18 08:15:05 -0300

    addcat

--- web/templates/balview_form.php
+++ web/templates/balview_form.php
@@ -42,23 +42,23 @@
 							<div class="row">
 							<div class="form-date-from form-icon">
 								<label for="date_from">From</label>
-								<input type="text" id="date_from" class="date_from" placeholder="Pick a date" />
+								<input type="text" id="inidate" class="date_from" placeholder="Pick a date" />
 								<!-- <span class="icon"><i class="zmdi zmdi-calendar-alt"></i></span> -->
 							</div>
 							<div class="form-date-to form-icon">
 								<label for="date_to">To</label>
-								<input type="text" id="date_to" class="date_to" placeholder="Pick a date" />
+								<input type="text" id="enddate" class="date_to" placeholder="Pick a date" />
 								<!-- <span class="icon"><i class="zmdi zmdi-calendar-alt"></i></span> -->
 							</div>
 								<div class="col-sm-6 form-group form-date-from">
 
 									<label for="inidate"> Initial Date:</label>
-									<input type="text" class="form-control date_from" id="inidate" name="inidate" value="<?php echo $my_inidate; ?>" required>
+									<input type="text" class="form-control date_from" id="date_from" name="inidate" value="<?php echo $my_inidate; ?>" required>
 								</div>
 								<div class="col-sm-6 form-group form-date-to">
 
 									<label for="enddate"> End Date:</label>
-									<input type="text" class="form-control date_to" id="enddate" name="enddate" value="<?php echo $my_enddate; ?>" required>
+									<input type="text" class="form-control date_to" id="date_to" name="enddate" value="<?php echo $my_enddate; ?>" required>
 								</div>
 							</div>
 							<div class="row">
