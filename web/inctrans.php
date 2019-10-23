ecad07b44af8956e6c784f6fbbaabbe08f8d2f5f
Salvatore <salvatore@byui.edu>
2019-10-23 19:52:44 -0300

    work please

--- web/inctrans.php
+++ web/inctrans.php
@@ -249,6 +249,23 @@ if (($is_create OR $is_update)) {
 							echo '</div>';	
 							echo '</div>';	
 }
+ ?>
+ <select class="image-picker show-html">
+  <optgroup label="Cats">
+    <option data-img-src="http://placekitten.com/220/200" value="1">Cute Kitten 1</option>
+    <option data-img-src="http://placekitten.com/180/200" value="2">Cute Kitten 2</option>
+    <option data-img-src="http://placekitten.com/130/200" value="3">Cute Kitten 3</option>
+    <option data-img-src="http://placekitten.com/270/200" value="4">Cute Kitten 4</option>
+  </optgroup>
+  <optgroup label="Animals">
+    <option data-img-src="http://lorempixel.com/220/200/animals/" value="5">Animal 1</option>
+    <option data-img-src="http://lorempixel.com/180/200/animals/" value="6">Animal 2</option>
+    <option data-img-src="http://lorempixel.com/130/200/animals/" value="7">Animal 3</option>
+    <option data-img-src="http://lorempixel.com/270/200/animals/" value="8">Animal 4</option>
+  </optgroup>
+</select>
+<?php
+
 	echo '</div>'; //end the navigation bar
 
 	echo '<div class="col-lg-3">';
