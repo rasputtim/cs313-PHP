b023e8e44d6a8d05dbf3f10c20a87d4d6ee45c0f
Salvatore <salvatore@byui.edu>
2019-10-23 08:12:54 -0300

    work please

--- web/templates/category_form.php
+++ web/templates/category_form.php
@@ -128,8 +128,12 @@
 							</div>
 							<div class="row">
 								<div class="col-sm-12 form-group">
-									<button type="submit" class="btn btn-lg btn-default pull-left" >Send &rarr;</button>
+									<button type="submit" class="button2" >Send &rarr;</button>
+									<?php
+										echo "<a class='button2' id= 'mylink' href='inccats.php?delete_data=".$id."'>DELETE</a>";
+									?>
 								</div>
+								
 							</div>
 						</form>
 								
