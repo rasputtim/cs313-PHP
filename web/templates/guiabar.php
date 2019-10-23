4020ec26a2aa2dd58e83741b7871581e2d52a315
Salvatore <salvatore@byui.edu>
2019-10-23 08:01:14 -0300

    work please

--- web/templates/guiabar.php
+++ web/templates/guiabar.php
@@ -2,7 +2,7 @@
 echo'<div class="container">';
 	echo'<div class="row">';
 	echo'<div class="col-lg-12">';
-	echo'<div class="breadcrumbs1_inner"><a href="index.php">home page</a>&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;&nbsp;add / edit category</div>	';
+	echo'<div class="breadcrumbs1_inner"><a href="index.php">home page</a>&nbsp;&nbsp;&nbsp;>&nbsp;&nbsp;&nbsp;'.$guiabar_ident.'</div>	';
 	echo'</div>	';
 	echo'</div>	';
 	echo'</div>	';
