77f1990f28735b1d8878393ed005dc394d57a64c
Salvatore <salvatore@byui.edu>
2019-10-23 17:32:51 -0300

    work please

--- web/inccats.php
+++ web/inccats.php
@@ -343,8 +343,27 @@ function showmydiv() {
 	}); //
 	$(window).load(function() {
 		//
-	
-	}); //
+	//Get a reference to the link on the page
+		// with an id of "mylink"
+		var a = document.getElementById("mylink");
+
+		//Set code to run when the link is clicked
+		// by assigning a function to "onclick"
+		a.onclick = function() {
+
+		// Your code here...
+
+		//If you don't want the link to actually 
+		// redirect the browser to another page,
+		// "google.com" in our example here, then
+		// return false at the end of this block.
+		// Note that this also prevents event bubbling,
+		// which is probably what we want here, but won't 
+		// always be the case.
+		message = 'Are you sure?';
+        return confirm(message);
+		}
+	});
 	</script>	
 </body>
 </html>
