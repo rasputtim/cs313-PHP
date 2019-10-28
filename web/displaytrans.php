76d3ad96762bb51fb55d5b91d9ac13b824931e7b
Salvatore <salvatore@byui.edu>
2019-10-28 20:59:18 -0300

    login

--- web/displaytrans.php
+++ web/displaytrans.php
@@ -40,25 +40,15 @@ include('templates/menubar.php');
 		?>
 		
 
-<div class="col-lg-9" id = "stage">
-	
-
-
-</div>
-<div class="col-lg-3">
-
-
-<?php 
-	include( 'templates/translist.php');
-
-?>
-	    	            		      	      			      
+	<div class="col-lg-9" id = "stage">
 		
 
-	
 
-	
-</div>	
+	</div>
+	<div class="col-lg-3">
+
+		
+	</div>	
 </div>
 <div class="row">
 <div class="col-lg-12">
@@ -84,10 +74,6 @@ include('templates/menubar.php');
 </div>	
 </div>	
 </div>
-
-
-
-	
 </div>
 <script type="text/javascript" src="js/bootstrap.js"></script>
 <script>
