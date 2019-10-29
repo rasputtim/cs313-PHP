458276367eea2c163663d4bfde25e866f72007e7
Salvatore <salvatore@byui.edu>
2019-10-29 15:20:24 -0300

    login

--- web/displaytrans.php
+++ web/displaytrans.php
@@ -84,7 +84,7 @@ include('templates/menubar.php');
     $( "#slider-range" ).slider({
       range: true,
       min: 0,
-      max: 10000000,
+      max: 1000000,
       values: [ 0, 0 ],
       slide: function( event, ui ) {
         $( "#text-amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
