cad15f43691f4a761feb7ce96844bbbd9ec4cd70
Salvatore <salvatore@byui.edu>
2019-10-29 15:07:03 -0300

    login

--- web/displaytrans.php
+++ web/displaytrans.php
@@ -87,10 +87,10 @@ include('templates/menubar.php');
       max: 500,
       values: [ 75, 300 ],
       slide: function( event, ui ) {
-        $( "#amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
+        $( "#text-amount" ).val( "$" + ui.values[ 0 ] + " - $" + ui.values[ 1 ] );
       }
     });
-    $( "#amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
+    $( "#text-amount" ).val( "$" + $( "#slider-range" ).slider( "values", 0 ) +
       " - $" + $( "#slider-range" ).slider( "values", 1 ) );
   } );
   </script>
