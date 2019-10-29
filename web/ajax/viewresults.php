7934ee74c1a37b89a216426e8ed16fcebfae2992
Salvatore <salvatore@byui.edu>
2019-10-29 07:50:37 -0300

    login

---
+++ web/ajax/viewresults.php
@@ -0,0 +1,220 @@
+<?php
+require_once ("../inc/functions_db.php");
+$money_format = '%(#10n';
+$date_format = "D, M d, Y ";
+
+$free_text = '';
+$start_date = '';
+$end_date = '';
+$status = -1;
+$user= '';
+$category = array();
+$amount = 0.00;
+$total = 0.0;
+   if( $_REQUEST["free_text"] ){
+      $free_text = $_REQUEST['free_text'];
+      if ($free_text == "Search-Text") $free_text = '';
+
+   }
+   if( $_REQUEST['start_date'] ){
+    $start_date = $_REQUEST['start_date'];
+       
+   }
+   if( $_REQUEST['end_date'] ){
+    $end_date = $_REQUEST['end_date'];
+        
+    }
+    if( $_REQUEST['status'] ){
+        $status = $_REQUEST['status'];
+        if ($status == 2) $status = 0;
+    }
+    if( $_REQUEST['user'] ){
+        $user= $_REQUEST['user'];
+       
+    //echo "user: " . $user;
+    }
+    if( $_REQUEST['category'] ){
+        $category =  $_REQUEST['category'];
+        foreach ($category as $row){
+            //echo "category: " . $row;
+            //echo " , ";
+         }
+    }
+    if( $_REQUEST['amount'] ){
+        $amount =  $_REQUEST['amount'];
+        if ($amount == "Amount") $amount = 0.00;
+    }
+
+    
+    
+	// Search filters
+    $start_date_sql = $start_date;
+    $end_date_sql = $end_date;
+    $user_id = $user;
+    
+	//Search filter processing
+
+	$sql_filter = "";
+    $date_filter = false;
+    $where_clause = "";
+    $where_saldo_inicial="";
+
+    $start_date_where="";
+    $end_date_where="";
+	if ($free_text != "") {
+		$sql_filter .= " AND (description LIKE '%$free_text%')";
+        $where_clause .= " AND (description LIKE '%$free_text%')";
+        $where_saldo_inicial .= " AND (description LIKE '%$free_text%')";
+	}
+	if ($user_id != 0) {
+	    $sql_filter .= " AND id_user = $user_id ";
+        $where_clause .= " AND id_user = $user_id ";
+        $where_saldo_inicial .= " AND id_user = $user_id ";
+    }
+	if ($category != 0){
+        
+
+		//todo: fiz sql for category
+
+		$filter = '';
+        $added = false;
+        if(!empty($category)) {
+            
+            if (count($category)==1){
+               if (!$category[0]==""){
+                $filter = 'AND (idcategory = '.$category[0];
+                $added = true;
+               }
+            }else {
+                $filter = 'AND (idcategory = '.$category[0];
+                $added = true;
+                $category2 = array_slice($category,1);
+                foreach($category2 as $rec_code){
+                    
+                $filter .= " OR idcategory=".$rec_code;
+                }
+            }
+		if ($added) $filter .= ")";
+        }
+		$sql_filter .= $filter;
+		$where_clause .= $filter;
+		$where_saldo_inicial .= $filter;
+
+	}
+
+	if ($start_date != "" AND $end_date == "") {
+		$sql_filter .= " AND duedate >= '$start_date_sql' ";
+        $date_filter = true;
+        $start_date_where = $start_date_sql;
+        $where_saldo_inicial .= " AND duedate < '$start_date_sql' ";
+        }
+
+	if ($end_date != "" AND $start_date == "") {
+		$sql_filter .= " AND duedate <= '$end_date_sql' ";
+		$date_filter = true;
+		$end_date_where = $end_date_sql;
+		}
+
+	if ($end_date != "" AND $start_date != "") {
+		$sql_filter .= " AND duedate BETWEEN  '$start_date_sql' AND '$end_date_sql'";
+		$date_filter = true;
+		$end_date_where = $end_date_sql;
+		$start_date_where = $start_date_sql;
+		$where_saldo_inicial .= " AND duedate < '$start_date_sql' ";
+
+	}
+    if($ammount != 0.00){
+        $sql_filter .= " AND amount = $amount ";
+		
+
+    }
+    if ($status != -1) {
+        $sql_filter .= " AND status = $status ";
+        
+    }
+
+    $offset = "100";
+
+    $sql1 = "SELECT * FROM public.ezfin_transactions  WHERE 1=1 $sql_filter ORDER BY  duedate, idcategory  LIMIT $offset";
+    //echo "SQL : " . $sql1;
+echo '<div class="col-lg-9" >';
+   echo '<h1>Transactions for the current Serch Criteria</h1>';
+
+
+   
+   
+   $stmt = get_db()->prepare($sql1);
+   //$stmt->bindValue(':op', $myOperation, PDO::PARAM_INT);
+   $stmt->execute();
+   $rows = $stmt->fetchAll(PDO::FETCH_ASSOC);
+   $count =0;
+   $added = false;
+   foreach ($rows as $row)
+   {
+       //get category
+       $oper_image ="cat_income_green_peq.png";
+       $stmt = get_db()->prepare('SELECT operation FROM public.ezfin_category WHERE idcat =  :op');
+       $stmt->bindValue(':op', $row['idcategory'], PDO::PARAM_INT);
+       $stmt->execute();
+       $operation = $stmt->fetchColumn();
+       switch($operation){
+          case 0:
+          $oper_image ="cat_income_green_peq.png";
+          break;
+          case 1:
+          $oper_image ="cat_bill_red_peq.png";
+          break;
+          case 2:
+          $oper_image = "cat_informative_peq.png";
+          break;
+       }
+       $added = false;
+       if ($count  == 0 ) echo '<ul class="thumbnails thumbnails1">';
+       $status_name = "Undefined";
+       switch ($row['status']){
+           case -1:
+           $status_name = "Undefined";
+           break;
+           case 0:
+           $status_name = "Unpaid/Unreceived";
+           break;
+           case 1:
+           $status_name = "Paid/Received";
+           break;
+       }
+       echo '<li>';
+           echo '<div class="thumbnail clearfix">';
+               // todo: add category icon here
+               echo '<figure class="oper_icon"><img src="images/'.$oper_image.'" alt=""></figure>';
+               echo '<div class="caption">';											
+                   echo '<h3> DUE ON: '.date_format(date_create($row['duedate']),$date_format)." - $ ". money_format($money_format, $row['amount']);
+                   echo '</h3>STATUS: '.$status_name;
+                   if ($status == 1){
+                       echo ' ON: '.$row['paymentdate'];
+                   }
+                   echo '<p>';
+                           echo "  ".$row['description']. '<a href=" inctrans.php?update='.$row['idtransaction'].'"><strong>  edit</strong></a>';
+                   echo '</p>';
+               echo '</div>';			
+           echo '</div>';
+       echo '</li>';
+       if ($count  == 1 ) {
+           echo '</ul>';
+           $count = 0;
+           $added = true;
+       }else $count ++;
+       $total += $row['amount'];
+   }
+   if ($added = false) echo '</ul>';
+   
+		
+
+
+echo '</div>';
+echo '<div class="col-lg-3"  >';
+      
+           echo "<h1>TOTAL AMOUNT: $ ". money_format($money_format, $total);
+           echo "</H1>";
+      
+echo '</div>';
+?> 
---
+++ web/displayviews.php
@@ -0,0 +1,109 @@
+<?php 
+session_start();
+
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    header("location:inc/noaccess.php");
+    exit;
+}
+require_once ("inc/functions_db.php");
+include('templates/header.php'); 
+
+$guiabar_ident = "display transactions by Periods";
+?>
+
+<body class="subpage">
+<div id="main">
+
+<div class="top1">
+<?php 
+$index1="false";
+$index2="true";
+$index3="false";
+$index4="false";
+$index5="false";
+include('templates/menubar.php'); 
+
+?>
+
+</div>
+
+<?php
+
+?>
+
+<div id="content">
+<div class="container">
+<div class="row">
+<?php
+		$search_type = "transactions";
+			include('templates/searchbyview.php'); 
+		?>
+</div>
+
+<div class="row" id = "stage">
+
+	<div class="col-lg-9" >
+		
+
+
+	</div>
+	<div class="col-lg-3"  >
+
+		
+	</div>	
+</div>
+<div class="row">
+<div class="col-lg-12">
+
+<div class="line1"></div>
+
+
+
+</div>	
+</div>	
+</div>	
+</div>
+
+
+<div class="bot1">
+<div class="container">
+<div class="row">
+<div class="col-lg-12">
+<div class="bot1_inner">
+
+	<?php include('templates/footer.php'); ?>
+	
+</div>	
+</div>	
+</div>
+</div>
+<script type="text/javascript" src="js/bootstrap.js"></script>
+<script src="js/mydatepick.js"></script>
+<script>
+	$(document).ready(function() {	
+		$("#driver").click(function(event){
+               var free_text = $("#text-free_text").val();
+			   var user = $("#text-user").val();
+			   var category = $("#category").val();
+			   var start_date = $("#text-start_date").val();
+			   var end_date = $("#text-end_date").val();
+			   var status = $(".statusclass:checked").val();
+			   var amount = $("#text-amount").val();
+               $("#stage").load('ajax/viewresults.php', {
+				   "free_text":free_text,
+				   "user":user,
+				   "category":category,
+				   "start_date":start_date,
+				   "end_date":end_date,
+				   "status":status,
+				   "amount":amount
+				   });
+            });
+	}); //
+	$(window).load(function() {
+		//
+		
+	}); //
+	</script>	
+</body>
+</html>
