526e7989d77a458d5422a586a791ee1eb3bc0c38
Salvatore <salvatore@byui.edu>
2019-10-31 17:41:37 -0300

    fix

--- web/listtrans.php
+++ web/listtrans.php
@@ -86,9 +86,9 @@ foreach ($rows as $row)
 			echo '<figure class="oper_icon"><img src="images/'.$oper_image.'" alt=""></figure>';
 			echo '<div class="caption">';											
 				echo "<h3>".$row['description'].'</H3>';
-				echo '<BR>DUE ON: '.date_format(date_create($row['duedate']),$date_format)." - $ ". money_format($money_format, $row['amount']);
-				echo '<SPAN><SPAN><a href="inctrans.php?update='.$row['idtransaction'].'" class="button2">edit </a>';
-				echo '</BR>';
+				echo '<H>DUE ON: '.date_format(date_create($row['duedate']),$date_format)." - $ ". money_format($money_format, $row['amount']);
+				echo '<a href="inctrans.php?update='.$row['idtransaction'].'" class="button2">edit </a>';
+				echo '</H>';
 			echo '</div>';			
 		echo '</div>';
 	echo '</li>';
