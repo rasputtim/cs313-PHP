4e3ce00e1b614d5c6e22adc4abd60739d3f24e82
Salvatore <salvatore@byui.edu>
2019-12-06 09:36:32 -0300

    adding req url to login

--- web/inc/noaccess.php
+++ web/inc/noaccess.php
@@ -7,7 +7,7 @@
 	echo "<img src='../images/noaccess.gif'>";
 	echo "<p>".'Access to this page is restricted to authorized users only, please contact system administrator if you need assistance. <br><br>Please know that all attempts to access this page are recorded in security logs of Integria System Database'. "</p>";
 	echo "</p>";
-	echo '<p> <a href="../index.php">Login</a></p>';
+	echo '<p> <a href="../index.php?redirurl='+$_SERVER['HTTP_REFERER']+'">Login</a></p>';
 	echo "</div>";
 	echo "</center>";
 	exit;
