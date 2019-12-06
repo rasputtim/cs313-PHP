1db37469420b9690f114d6cdcb8b7ba024055bec
Salvatore <salvatore@byui.edu>
2019-12-06 09:42:35 -0300

    adding req url to login

--- web/inc/noaccess.php
+++ web/inc/noaccess.php
@@ -1,5 +1,7 @@
 <?php
-	
+	$redirurl = $_SERVER['HTTP_REFERER'];
+	if ($_REQUEST['redirurl'] == "https://cryptic-beyond-10470.herokuapp.com/")
+	$redirurl = "https://cryptic-beyond-10470.herokuapp.com/index.php";
 	echo "<center>";
 	echo '<div style="margin-top: 100px; width: 450px;">';
 	echo '<h2>'.'You don\'t have access to this page'.'</h2>';
@@ -7,7 +9,7 @@
 	echo "<img src='../images/noaccess.gif'>";
 	echo "<p>".'Access to this page is restricted to authorized users only, please contact system administrator if you need assistance. <br><br>Please know that all attempts to access this page are recorded in security logs of Integria System Database'. "</p>";
 	echo "</p>";
-	echo '<p> <a href="../index.php?redirurl='+$_SERVER['HTTP_REFERER']+'">Login</a></p>';
+	echo "<p> <a href=$redirurl>Login</a></p>";
 	echo "</div>";
 	echo "</center>";
 	exit;
